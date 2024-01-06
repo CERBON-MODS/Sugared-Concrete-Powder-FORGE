beb64b2b9206df29f179431ddce478a01a81cc06
Felipe <felipecerboncinix@gmail.com>
2024-01-05 23:24:45 -0300

    Put colors in front of the names to keep parity with Minecraft

--- src/main/java/com/cerbon/sugared_concrete/datagen/providers/SCLanguageProvider.java
+++ src/main/java/com/cerbon/sugared_concrete/datagen/providers/SCLanguageProvider.java
@@ -13,21 +13,21 @@ public class SCLanguageProvider extends LanguageProvider {
 
     @Override
     protected void addTranslations() {
-        add(SCBlocks.SUGARED_WHITE_CONCRETE_POWDER.get(), "Sugared White Concrete Powder");
-        add(SCBlocks.SUGARED_ORANGE_CONCRETE_POWDER.get(), "Sugared Orange Concrete Powder");
-        add(SCBlocks.SUGARED_MAGENTA_CONCRETE_POWDER.get(), "Sugared Magenta Concrete Powder");
-        add(SCBlocks.SUGARED_LIGHT_BLUE_CONCRETE_POWDER.get(), "Sugared Light Blue Concrete Powder");
-        add(SCBlocks.SUGARED_YELLOW_CONCRETE_POWDER.get(), "Sugared Yellow Concrete Powder");
-        add(SCBlocks.SUGARED_LIME_CONCRETE_POWDER.get(), "Sugared Lime Concrete Powder");
-        add(SCBlocks.SUGARED_PINK_CONCRETE_POWDER.get(), "Sugared Pink Concrete Powder");
-        add(SCBlocks.SUGARED_GRAY_CONCRETE_POWDER.get(), "Sugared Gray Concrete Powder");
-        add(SCBlocks.SUGARED_LIGHT_GRAY_CONCRETE_POWDER.get(), "Sugared Light Gray Concrete Powder");
-        add(SCBlocks.SUGARED_CYAN_CONCRETE_POWDER.get(), "Sugared Cyan Concrete Powder");
-        add(SCBlocks.SUGARED_PURPLE_CONCRETE_POWDER.get(), "Sugared Purple Concrete Powder");
-        add(SCBlocks.SUGARED_BLUE_CONCRETE_POWDER.get(), "Sugared Blue Concrete Powder");
-        add(SCBlocks.SUGARED_BROWN_CONCRETE_POWDER.get(), "Sugared Brown Concrete Powder");
-        add(SCBlocks.SUGARED_GREEN_CONCRETE_POWDER.get(), "Sugared Green Concrete Powder");
-        add(SCBlocks.SUGARED_RED_CONCRETE_POWDER.get(), "Sugared Red Concrete Powder");
-        add(SCBlocks.SUGARED_BLACK_CONCRETE_POWDER.get(), "Sugared Black Concrete Powder");
+        add(SCBlocks.SUGARED_WHITE_CONCRETE_POWDER.get(), "White Sugared Concrete Powder");
+        add(SCBlocks.SUGARED_ORANGE_CONCRETE_POWDER.get(), "Orange Sugared Concrete Powder");
+        add(SCBlocks.SUGARED_MAGENTA_CONCRETE_POWDER.get(), "Magenta Sugared Concrete Powder");
+        add(SCBlocks.SUGARED_LIGHT_BLUE_CONCRETE_POWDER.get(), "Light Blue Sugared Concrete Powder");
+        add(SCBlocks.SUGARED_YELLOW_CONCRETE_POWDER.get(), "Yellow Sugared Concrete Powder");
+        add(SCBlocks.SUGARED_LIME_CONCRETE_POWDER.get(), "Lime Sugared Concrete Powder");
+        add(SCBlocks.SUGARED_PINK_CONCRETE_POWDER.get(), "Pink Sugared Concrete Powder");
+        add(SCBlocks.SUGARED_GRAY_CONCRETE_POWDER.get(), "Gray Sugared Concrete Powder");
+        add(SCBlocks.SUGARED_LIGHT_GRAY_CONCRETE_POWDER.get(), "Light Gray Sugared Concrete Powder");
+        add(SCBlocks.SUGARED_CYAN_CONCRETE_POWDER.get(), "Cyan Sugared Concrete Powder");
+        add(SCBlocks.SUGARED_PURPLE_CONCRETE_POWDER.get(), "Purple Sugared Concrete Powder");
+        add(SCBlocks.SUGARED_BLUE_CONCRETE_POWDER.get(), "Blue Sugared Concrete Powder");
+        add(SCBlocks.SUGARED_BROWN_CONCRETE_POWDER.get(), "Brown Sugared Concrete Powder");
+        add(SCBlocks.SUGARED_GREEN_CONCRETE_POWDER.get(), "Green Sugared Concrete Powder");
+        add(SCBlocks.SUGARED_RED_CONCRETE_POWDER.get(), "Red Sugared Concrete Powder");
+        add(SCBlocks.SUGARED_BLACK_CONCRETE_POWDER.get(), "Black Sugared Concrete Powder");
     }
 }
