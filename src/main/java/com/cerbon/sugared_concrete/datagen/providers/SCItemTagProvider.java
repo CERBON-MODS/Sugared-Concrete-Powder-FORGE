0ba5c1565d7193c384f97ed6eb0aff93cf31e05f
Felipe <felipecerboncinix@gmail.com>
2024-01-05 23:09:59 -0300

    Fix recipes and tags

--- src/main/java/com/cerbon/sugared_concrete/datagen/providers/SCItemTagProvider.java
+++ src/main/java/com/cerbon/sugared_concrete/datagen/providers/SCItemTagProvider.java
@@ -1,15 +1,17 @@
 package com.cerbon.sugared_concrete.datagen.providers;
 
 import com.cerbon.sugared_concrete.SugaredConcrete;
-import com.cerbon.sugared_concrete.block.SCBlocks;
 import com.cerbon.sugared_concrete.util.SCTags;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.ItemTagsProvider;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.ConcretePowderBlock;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.ForgeRegistries;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 
@@ -21,6 +23,9 @@ public class SCItemTagProvider extends ItemTagsProvider {
 
     @Override
     protected void addTags(HolderLookup.@NotNull Provider provider) {
-        SCBlocks.BLOCKS.getEntries().forEach(block -> this.tag(SCTags.CONCRETE_POWDERS).add(block.get().asItem()));
+        ForgeRegistries.BLOCKS.getEntries().stream()
+                .map(Map.Entry::getValue)
+                .filter(ConcretePowderBlock.class::isInstance)
+                .forEach(block -> this.tag(SCTags.CONCRETE_POWDERS).add(block.asItem()));
     }
 }
--- src/main/java/com/cerbon/sugared_concrete/datagen/providers/SCRecipeProvider.java
+++ src/main/java/com/cerbon/sugared_concrete/datagen/providers/SCRecipeProvider.java
@@ -9,6 +9,7 @@ import net.minecraft.data.recipes.RecipeProvider;
 import net.minecraft.data.recipes.ShapedRecipeBuilder;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.crafting.conditions.IConditionBuilder;
 import org.jetbrains.annotations.NotNull;
 
@@ -22,15 +23,30 @@ public class SCRecipeProvider extends RecipeProvider implements IConditionBuilde
 
     @Override
     protected void buildRecipes(@NotNull Consumer<FinishedRecipe> writer) {
-        SCBlocks.BLOCKS.getEntries().forEach(block -> sugaredConcretePowder(writer, block.get()));
+        sugaredConcretePowder(writer, Blocks.WHITE_CONCRETE_POWDER, SCBlocks.SUGARED_WHITE_CONCRETE_POWDER.get());
+        sugaredConcretePowder(writer, Blocks.LIGHT_GRAY_CONCRETE_POWDER, SCBlocks.SUGARED_LIGHT_GRAY_CONCRETE_POWDER.get());
+        sugaredConcretePowder(writer, Blocks.GRAY_CONCRETE_POWDER, SCBlocks.SUGARED_GRAY_CONCRETE_POWDER.get());
+        sugaredConcretePowder(writer, Blocks.BLACK_CONCRETE_POWDER, SCBlocks.SUGARED_BLACK_CONCRETE_POWDER.get());
+        sugaredConcretePowder(writer, Blocks.BROWN_CONCRETE_POWDER, SCBlocks.SUGARED_BROWN_CONCRETE_POWDER.get());
+        sugaredConcretePowder(writer, Blocks.RED_CONCRETE_POWDER, SCBlocks.SUGARED_RED_CONCRETE_POWDER.get());
+        sugaredConcretePowder(writer, Blocks.ORANGE_CONCRETE_POWDER, SCBlocks.SUGARED_ORANGE_CONCRETE_POWDER.get());
+        sugaredConcretePowder(writer, Blocks.YELLOW_CONCRETE_POWDER, SCBlocks.SUGARED_YELLOW_CONCRETE_POWDER.get());
+        sugaredConcretePowder(writer, Blocks.LIME_CONCRETE_POWDER, SCBlocks.SUGARED_LIME_CONCRETE_POWDER.get());
+        sugaredConcretePowder(writer, Blocks.GREEN_CONCRETE_POWDER, SCBlocks.SUGARED_GREEN_CONCRETE_POWDER.get());
+        sugaredConcretePowder(writer, Blocks.CYAN_CONCRETE_POWDER, SCBlocks.SUGARED_CYAN_CONCRETE_POWDER.get());
+        sugaredConcretePowder(writer, Blocks.LIGHT_BLUE_CONCRETE_POWDER, SCBlocks.SUGARED_LIGHT_BLUE_CONCRETE_POWDER.get());
+        sugaredConcretePowder(writer, Blocks.BLUE_CONCRETE_POWDER, SCBlocks.SUGARED_BLUE_CONCRETE_POWDER.get());
+        sugaredConcretePowder(writer, Blocks.PURPLE_CONCRETE_POWDER, SCBlocks.SUGARED_PURPLE_CONCRETE_POWDER.get());
+        sugaredConcretePowder(writer, Blocks.MAGENTA_CONCRETE_POWDER, SCBlocks.SUGARED_MAGENTA_CONCRETE_POWDER.get());
+        sugaredConcretePowder(writer, Blocks.PINK_CONCRETE_POWDER, SCBlocks.SUGARED_PINK_CONCRETE_POWDER.get());
     }
 
-    private void sugaredConcretePowder(Consumer<FinishedRecipe> finishedRecipeConsumer, Block block) {
-        ShapedRecipeBuilder.shaped(RecipeCategory.BUILDING_BLOCKS, block, 8)
+    private void sugaredConcretePowder(Consumer<FinishedRecipe> finishedRecipeConsumer, Block concretePowder, Block result) {
+        ShapedRecipeBuilder.shaped(RecipeCategory.BUILDING_BLOCKS, result, 8)
                 .pattern("ccc")
                 .pattern("csc")
                 .pattern("ccc")
-                .define('c', block.asItem())
+                .define('c', concretePowder)
                 .define('s', Items.SUGAR)
                 .group("sugared_concrete_powder")
                 .unlockedBy("has_concrete_powder", has(SCTags.CONCRETE_POWDERS))
